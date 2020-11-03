2f403c4fc7e74fbe2efb0c93904f22a06c0a84e0
Mateusz Chilinski <mateuszchilinski@k165.mini.pw.edu.pl>
2020-11-03 15:42:10 +0100

    merge_interactions should be working initial work on similarity matrix

---
+++ generate_similarity_matrix.py
@@ -0,0 +1,66 @@
+# script for generating histograms for anchor & loop (interaction) lengths
+import subprocess
+import numpy as np
+import math
+import pandas as pd
+import shutil
+import shlex
+import os
+from matplotlib import pyplot as plt
+from os import listdir
+from os.path import isfile, join
+from collections import defaultdict
+def run_comparison(file1, file2):
+    cmd = "cat " + file1 + " | wc -l"
+    reference_count = int(subprocess.getoutput(cmd))
+    cmd = "pairToPair -a "+file1+" -b "+file2+" | wc -l"
+    common_count = int(subprocess.getoutput(cmd))
+    return str(round(common_count/reference_count*100, 1))+"%"
+
+def get_counts(file):
+    cmd = "cat " + file + " | wc -l"
+    reference_count = int(subprocess.getoutput(cmd))
+    return str(reference_count)
+
+def create_loops(file, folder):
+    loop_command = 'python /mnt/raid/ctcf_prediction_anal/cluster-paired-end-tags/cluster_pets/cluster_PETs.py --pets_filename '+file+' --clusters_filename '+folder+file.split("/")[-1]
+    output = subprocess.getoutput(loop_command)
+
+def generate_matrix(folder_to_compare):
+
+    files_to_compare = [folder_to_compare+f for f in listdir(folder_to_compare) if isfile(join(folder_to_compare, f))]
+    matrix = defaultdict(dict)
+    for file1 in files_to_compare:
+        for file2 in files_to_compare:
+            matrix[file1.split("/")[-1]][file2.split("/")[-1]] = run_comparison(file1, file2)
+
+    df = pd.DataFrame(matrix).T
+    df = pd.concat(
+        [pd.concat(
+            [df],
+            keys=['File'], axis=1)],
+        keys=['Reference']
+    )
+
+    df = df.sort_index().sort_index(axis = 1)
+    print(df)
+
+    print("")
+    print("Counts of interactions in files:")
+
+    for file in files_to_compare:
+        print(file.split("/")[-1] + ": " + get_counts(file))
+
+folder_to_compare = '/mnt/raid/ctcf_prediction_anal/trios_new_ctcf/ctcf_named_2/'
+generate_matrix(folder_to_compare)
+
+if os.path.exists(folder_to_compare+"temp") and os.path.isdir(folder_to_compare+"temp"):
+    shutil.rmtree(folder_to_compare+"temp")
+    os.mkdir(folder_to_compare+"temp")
+
+files_to_compare = [folder_to_compare+f for f in listdir(folder_to_compare) if isfile(join(folder_to_compare, f))]
+for file in files_to_compare:
+    create_loops(file, folder_to_compare+"temp")
+generate_matrix(folder_to_compare+"temp")
+
+shutil.rmtree(folder_to_compare+"temp")
---
+++ merge_interactions_replicates.py
@@ -0,0 +1,100 @@
+import argparse
+from sortedcontainers import SortedList
+import progressbar
+
+def checkOverlap(interaction1, interaction2):
+        if(interaction1.chr1 != interaction2.chr1 or interaction1.chr2 != interaction2.chr2):
+            return False
+
+        if(max(interaction1.pos1, interaction2.pos1) <= min(interaction1.end1, interaction2.end1)):
+            if(max(interaction1.pos2, interaction2.pos2) <= min(interaction1.end2, interaction2.end2)):
+                return True
+        return False
+
+def loadInteractions(fileName):
+    interactions = SortedList([], key=lambda x: (x.chr1, x.pos1, x.end1, x.chr2, x.pos2, x.end2))
+    with open(fileName, 'r') as f: #open the file
+        print("Parsing file: " + fileName+"...")
+        lines = f.readlines()
+        bar = progressbar.ProgressBar(maxval=len(lines), widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+        i = 0
+        bar.start()
+        for line in lines:
+            bar.update(i+1)
+            i = i + 1
+            values = line.split("\t")
+            interaction = Interaction(values[0], int(values[1]), int(values[2]), values[3], int(values[4]), int(values[5]), int(values[6]))
+            interactions.add(interaction)
+        bar.finish()
+        return interactions
+class Interaction:
+    def __init__(self, chr1, pos1, end1, chr2, pos2, end2, pet):
+        self.chr1 = chr1
+        self.chr2 = chr2
+        self.pos1 = pos1
+        self.pos2 = pos2
+        self.end1 = end1
+        self.end2 = end2
+        self.pet = pet
+    def __eq__(self, other):
+        """Overrides the default implementation"""
+        if isinstance(other, Interaction):
+            return self.chr1 == other.chr1 and self.pos1 == other.pos1 and self.end1 == other.end1 and self.chr2 == other.chr2 and self.pos2 == other.pos2 and self.end2 == other.end2
+        return False
+    def generateLine(self):
+        return self.chr1+"\t"+str(self.pos1)+"\t"+str(self.end1)+"\t"+self.chr2+"\t"+str(self.pos2)+"\t"+str(self.end2)+"\t"+str(self.pet)+"\n"
+
+parser = argparse.ArgumentParser()
+parser.add_argument('interactions_files', metavar='file', help="Files containing ", nargs='+')
+
+args = parser.parse_args()
+
+if(len(args.interactions_files) < 2):
+    print("Merging one file makes no sense, does it?")
+    exit(1)
+
+interactionsReplicates = list()
+
+for file in args.interactions_files:
+    interactions = loadInteractions(file)
+    interactionsReplicates.append(interactions)
+edited = 0
+allInteractions = interactionsReplicates[0]
+fileId = 2
+for interactionsReplicate in interactionsReplicates[1:]:
+    print("Merging interactions from file #" + str(fileId))
+    bar = progressbar.ProgressBar(maxval=len(interactionsReplicate), widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+    bar.start()
+    i = 0
+    for interaction in interactionsReplicate:
+        bar.update(i + 1)
+        i = i + 1
+        beforeInteraction = allInteractions.bisect_left(Interaction(interaction.chr1, interaction.pos1-5000, interaction.pos1, interaction.chr1, interaction.pos1, interaction.pos1, interaction.pet))
+        if(beforeInteraction > len(allInteractions)-1): # is after all, just to be sure
+            allInteractions.add(interaction)
+            continue
+        afterInteraction = allInteractions.bisect_right(Interaction(interaction.chr1, interaction.end1+5000, interaction.end1, interaction.chr1, interaction.end1, interaction.end1, interaction.pet))
+        
+        found_interaction = False
+        for interactionId in range(beforeInteraction, afterInteraction+1):
+            if(checkOverlap(allInteractions[interactionId], interaction)): # check if overlaps on both sides
+                allInteractions[interactionId].pet += interaction.pet
+                # merge anchors
+                edited += 1
+                allInteractions[interactionId].pos1 = min(allInteractions[interactionId].pos1, interaction.pos1)
+                allInteractions[interactionId].end1 = max(allInteractions[interactionId].end1, interaction.end1)
+
+                allInteractions[interactionId].pos2 = min(allInteractions[interactionId].pos2, interaction.pos2)
+                allInteractions[interactionId].end2 = max(allInteractions[interactionId].end2, interaction.end2)
+
+                found_interaction = True
+                break
+        if not found_interaction: # it does not overlap with any, lets add it as independent one
+            allInteractions.add(interaction)
+    bar.finish()
+print(str(edited))
+with open('result.bedpe', 'w') as f:
+    for interaction in allInteractions:
+        f.write(interaction.generateLine())
+
+print("Finished parsing!")
