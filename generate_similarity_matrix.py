fbac5c1b17bff66e6af32fea51ee1200e771b2dc
Mateusz Chilinski <mateuszchilinski@k165.mini.pw.edu.pl>
2020-11-09 15:40:07 +0100

    added peaks

--- generate_similarity_matrix.py
+++ generate_similarity_matrix.py
@@ -25,11 +25,26 @@ class Interaction:
     def generateLine(self):
         return self.chr1+"\t"+str(self.pos1)+"\t"+str(self.end1)+"\t"+self.chr2+"\t"+str(self.pos2)+"\t"+str(self.end2)+"\t"+str(self.pet)+"\n"
 
+def createFolder(folder):
+    if os.path.exists(folder) and os.path.isdir(folder):
+        shutil.rmtree(folder)
+    os.mkdir(folder)
+def removeFolder(folder):
+    if os.path.exists(folder) and os.path.isdir(folder):
+        shutil.rmtree(folder)
 def run_comparison(files):
     file1, file2 = files
     cmd = "cat " + file1 + " | wc -l"
     reference_count = int(subprocess.getoutput(cmd))
     cmd = "pairToPair -a "+file1+" -b "+file2+" | wc -l"
+    common_count = int(subprocess.getoutput(cmd))
+    return str(round(common_count/reference_count*100, 1))+"%"
+
+def run_comparison_bed(files):
+    file1, file2 = files
+    cmd = "cat " + file1 + " | wc -l"
+    reference_count = int(subprocess.getoutput(cmd))
+    cmd = "bedtools intersect -wa -a "+file1+" -b "+file2+" | wc -l"
     common_count = int(subprocess.getoutput(cmd))
     return str(round(common_count/reference_count*100, 1))+"%"
 
@@ -51,18 +66,34 @@ def create_loops(file, folder, peaks=False):
         output = subprocess.getoutput("cut -f1-7 " + fileName+"_2 > " + fileName)
         os.remove(fileName+"_2")
 
+def enlarge_anchors(folder_to_compare, enlargeAnchors):
+    files_to_enlarge = [folder_to_compare+f for f in listdir(folder_to_compare) if isfile(join(folder_to_compare, f)) and f.split(".")[-1] == "bedpe"]
+    for file in files_to_enlarge:
+        interactions = loadInteractions(file)
+        for interaction in interactions:
+            interaction.pos1 -= enlargeAnchors
+            interaction.pos2 -= enlargeAnchors
+            interaction.end1 += enlargeAnchors
+            interaction.end2 += enlargeAnchors
+        fileName = folder_to_compare+"enlarged/"+file.split("/")[-1]
+        with open(fileName, 'w') as f:
+            for interaction in interactions:
+                f.write(interaction.generateLine())
 
-
-def generate_matrix(folder_to_compare):
+def generate_matrix(folder_to_compare, enlargeAnchors=0, func_to_use=run_comparison, ext="bedpe"):
+    if enlargeAnchors > 0:
+        createFolder(folder_to_compare+"enlarged/")
+        enlarge_anchors(folder_to_compare, enlargeAnchors)
+        folder_to_compare += "enlarged/"
     threads = 16
-    files_to_compare = [folder_to_compare+f for f in listdir(folder_to_compare) if isfile(join(folder_to_compare, f)) and f.split(".")[-1] == "bedpe"]
+    files_to_compare = [folder_to_compare+f for f in listdir(folder_to_compare) if isfile(join(folder_to_compare, f)) and f.split(".")[-1] == ext]
     matrix = defaultdict(dict)
     task_list = list()
     for file1 in files_to_compare:
         for file2 in files_to_compare:
             task_list.append((file1, file2))
     pool = mp.Pool(threads)
-    results = pool.map(run_comparison, task_list)
+    results = pool.map(func_to_use, task_list)
     pool.close()
     pool.join() 
     i = 0
@@ -118,53 +149,51 @@ start_time = time.time()
 
 folder_to_compare = '/mnt/raid/ctcf_prediction_anal/trios_new_ctcf/ctcf_named/output/'
 randomSampling = True
+enlargeAnchors = 500 # 0 = disabled
 #folder_to_compare = '/mnt/raid/ctcf_prediction_anal/trios_new_ctcf/ctcf_named/output/'
 rs_temp = ""
-if(randomSampling):
-    print("===== SAMPLING IS ON =====")
-    if os.path.exists(folder_to_compare+"rs_temp") and os.path.isdir(folder_to_compare+"rs_temp"):
-        shutil.rmtree(folder_to_compare+"rs_temp")
-    os.mkdir(folder_to_compare+"rs_temp")
-    files_to_compare = [folder_to_compare+f for f in listdir(folder_to_compare) if isfile(join(folder_to_compare, f)) and f.split(".")[-1] == "bedpe"]
-    files_to_copy = [folder_to_compare+f for f in listdir(folder_to_compare) if isfile(join(folder_to_compare, f)) and f.split(".")[-1] == "bed"]
 
-    for file in files_to_compare:
-        createRandomSample(file, folder_to_compare+"rs_temp/", 230000)
+print("===== PEAKS =====")
+
+generate_matrix(folder_to_compare,0,run_comparison_bed, "bed")
+
+if randomSampling:
     rs_temp = "rs_temp/"
+    print("===== SAMPLING IS ON =====")
+    createFolder(folder_to_compare+rs_temp)
+    files_to_copy = [folder_to_compare+f for f in listdir(folder_to_compare) if isfile(join(folder_to_compare, f)) and f.split(".")[-1] == "bed"]
+    for file in files_to_copy:
+        shutil.copy2(file, folder_to_compare+rs_temp+file.split("/")[-1])
+
+    files_to_compare = [folder_to_compare+f for f in listdir(folder_to_compare) if isfile(join(folder_to_compare, f)) and f.split(".")[-1] == "bedpe"]
+    for file in files_to_compare:
+        createRandomSample(file, folder_to_compare+rs_temp, 200000)
 print("===== INTERACTIONS =====")
-generate_matrix(folder_to_compare+rs_temp)
+generate_matrix(folder_to_compare+rs_temp, enlargeAnchors)
 
-if os.path.exists(folder_to_compare+"temp") and os.path.isdir(folder_to_compare+"temp"):
-    shutil.rmtree(folder_to_compare+"temp")
-os.mkdir(folder_to_compare+"temp")
+createFolder(folder_to_compare+rs_temp+"temp")
+createFolder(folder_to_compare+rs_temp+"temp2")
 
-if os.path.exists(folder_to_compare+"temp2") and os.path.isdir(folder_to_compare+"temp2"):
-    shutil.rmtree(folder_to_compare+"temp2")
-os.mkdir(folder_to_compare+"temp2")
-
-files_to_compare = [folder_to_compare+f for f in listdir(folder_to_compare) if isfile(join(folder_to_compare, f)) and f.split(".")[-1] == "bedpe"]
+files_to_compare = [folder_to_compare+rs_temp+f for f in listdir(folder_to_compare+rs_temp) if isfile(join(folder_to_compare+rs_temp, f)) and f.split(".")[-1] == "bedpe"]
 
 for file in files_to_compare:
-    create_loops(file, folder_to_compare+"temp/", False)
+    create_loops(file, folder_to_compare+rs_temp+"temp/", False)
     if(randomSampling):
-        createRandomSample(file, folder_to_compare+"temp/", 20000)
+        createRandomSample(file, folder_to_compare+rs_temp+"temp/", 20000)
 print("===== LOOPS (NO PEAKS) =====")
-generate_matrix(folder_to_compare+"temp/")
+generate_matrix(folder_to_compare+rs_temp+"temp/", enlargeAnchors)
 for file in files_to_compare:
     if(os.path.isfile(os.path.splitext(file)[0]+".bed")): 
-        create_loops(file, folder_to_compare+"temp2/", True)
+        create_loops(file, folder_to_compare+rs_temp+"temp2/", True)
         if(randomSampling):
-            createRandomSample(file, folder_to_compare+"temp2/", 10000)
+            createRandomSample(file, folder_to_compare+rs_temp+"temp2/", 10000)
 print("===== LOOPS (PEAKS) =====")
-generate_matrix(folder_to_compare+"temp2/")
+generate_matrix(folder_to_compare+rs_temp+"temp2/", enlargeAnchors)
 
 print("--- Executed in %s seconds ---" % (time.time() - start_time))
 
-if os.path.exists(folder_to_compare+"temp") and os.path.isdir(folder_to_compare+"temp"):
-    shutil.rmtree(folder_to_compare+"temp")
-if os.path.exists(folder_to_compare+"temp2") and os.path.isdir(folder_to_compare+"temp2"):
-    shutil.rmtree(folder_to_compare+"temp2")
+removeFolder(folder_to_compare+rs_temp+"temp")
+removeFolder(folder_to_compare+rs_temp+"temp2")
 
-if(randomSampling):
-    if os.path.exists(folder_to_compare+"rs_temp") and os.path.isdir(folder_to_compare+"rs_temp"):
-        shutil.rmtree(folder_to_compare+"rs_temp")
+if randomSampling or enlargeAnchors > 0:
+    removeFolder(folder_to_compare+rs_temp)
