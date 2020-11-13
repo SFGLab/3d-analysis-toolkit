1ed7235bf5f42aabd544e176edb7d4819dcb5446
Mateusz Chilinski <mateuszchilinski@k165.mini.pw.edu.pl>
2020-11-13 15:39:53 +0100

    working version with parallelism

--- generate_venn.py
+++ generate_venn.py
@@ -5,6 +5,8 @@ import math
 import time
 from sortedcontainers import SortedList
 import itertools
+import asyncio
+import multiprocessing as mp
 
 class Interaction:
     def __init__(self, chr1, pos1, end1, chr2, pos2, end2, pet):
@@ -20,10 +22,12 @@ class Interaction:
     def __eq__(self, other):
         """Overrides the default implementation"""
         if isinstance(other, Interaction):
-            return self.chr1 == other.chr1 and self.pos1 == other.pos1 and self.end1 == other.end1 and self.chr2 == other.chr2 and self.pos2 == other.pos2 and self.end2 == other.end2
+            return checkOverlap(self, other)
         return False
+    def __hash__(self):
+        return hash((self.chr1, self.chr2, self.pos1, self.pos2, self.end1, self.end2))
 def checkOverlap(interaction1, interaction2):
-    extension = -1
+    extension = -1 # -1 added for actuall 1bp overlap
     if(interaction1.chr1 != interaction2.chr1 or interaction1.chr2 != interaction2.chr2):
         return False
     if(max(interaction1.pos1, interaction2.pos1)-extension <= min(interaction1.end1, interaction2.end1)):
@@ -31,8 +35,11 @@ def checkOverlap(interaction1, interaction2):
             return True
     return False
 
+def orderInt(x):
+    return (x.chr1, x.pos1, x.end1, x.chr2, x.pos2, x.end2)
+
 def loadInteractions(fileName):
-    interactions = SortedList([], key=lambda x: (x.chr1, x.pos1, x.end1, x.chr2, x.pos2, x.end2))
+    interactions = SortedList([], key=orderInt)
     with open(fileName, 'r') as f: #open the file
         lines = f.readlines()
         for line in lines:
@@ -41,17 +48,40 @@ def loadInteractions(fileName):
             interactions.add(interaction)
     return interactions
 
-def getOverlappingMultiple(argv):
-    if(len(argv) == 0):
-        return list()
-    interactions = argv[0]
-    if(len(argv) > 1):
-        for arg in argv[1:]:
-            interactions = getOverlapping(interactions, arg)
+def getSet(args):
+    interactions_all, comb = args
+    interactions = SortedList([], key=lambda x: (x.chr1, x.pos1, x.end1, x.chr2, x.pos2, x.end2))
+    i = 0
+    # needed twice because first intersect, then removal
+    for c_i in comb:
+        if(c_i == 1):
+            interactions = getOverlapping(interactions, interactions_all[i])
+        i += 1
+    i = 0
+    for c_i in comb:
+        if(c_i == 0):
+            interactions = removeOverlapping(interactions, interactions_all[i])
+        i += 1
+
+    seen = set()
+    seen_add = seen.add
+    return ("".join(map(str, comb)), [x for x in interactions if not (x in seen or seen_add(x))])
+
+def removeOverlapping(interactions1, interactions2): # subtract I1-I2
+    interactions = SortedList([], key=lambda x: (x.chr1, x.pos1, x.end1, x.chr2, x.pos2, x.end2))
+    for interaction in interactions1:
+        interactions.add(interaction)
+    to_remove = getOverlapping(interactions, interactions2, True)
+    for interaction in to_remove:
+        interactions.discard(interaction)
     return interactions
 
-def getOverlapping(interactions1, interactions2):
-    interactions = list()
+def getOverlapping(interactions1, interactions2, toRemove=False): # intersection
+    interactions = SortedList([], key=lambda x: (x.chr1, x.pos1, x.end1, x.chr2, x.pos2, x.end2))
+    if(toRemove):
+        interactions = list()
+    if(len(interactions1) == 0):
+        return interactions2
     for interaction1 in interactions1:
         beforeInteraction = interactions2.bisect_left(Interaction(interaction1.chr1, interaction1.pos1-20000, interaction1.pos1, interaction1.chr1, interaction1.pos1, interaction1.pos1, interaction1.pet))
         afterInteraction = interactions2.bisect_right(Interaction(interaction1.chr1, interaction1.end1+20000, interaction1.end1, interaction1.chr1, interaction1.end1, interaction1.end1, interaction1.pet))
@@ -63,60 +93,45 @@ def getOverlapping(interactions1, interactions2):
                 break
             if not checkOverlap(interaction1, interaction2):
                 continue
-            interactions.append(Interaction(interaction1.chr1, min(interaction1.pos1, interaction2.pos1), max(interaction1.end1, interaction2.end1),
-            interaction1.chr2, min(interaction1.pos2, interaction2.pos2), max(interaction1.end2, interaction2.end2), interaction1.pet+interaction2.pet))
+            if not(toRemove):
+                interactions.add(Interaction(interaction1.chr1, interaction1.pos1, interaction1.end1,
+            interaction1.chr2, interaction1.pos2, interaction1.end2, interaction1.pet+interaction2.pet))
+            else:
+                interactions.append(interaction1)
+            #interactions.add(Interaction(interaction1.chr1, min(interaction1.pos1, interaction2.pos1), max(interaction1.end1, interaction2.end1),
+            #interaction1.chr2, min(interaction1.pos2, interaction2.pos2), max(interaction1.end2, interaction2.end2), interaction1.pet+interaction2.pet))
     return interactions
 start_time = time.time()
 
-def compare_touples4(t1, t2):
-    counter = 0
-    for i in range(0,4):
-        if(t1[i] == 1 and t1[i] == t2[i]):
-            counter += 1
-    return counter
 
-def sum_tuple(t):
-    c = 0
-    for item in t:
-        c += item
-    return c
+start_time = time.time()
 interactions_all = dict()
 
-interactions_all["NA19238"] = loadInteractions("/mnt/raid/ctcf_prediction_anal/trios_new_ctcf/ctcf_named/output/NA19238.bedpe") # 1
-interactions_all["NA19239"] = loadInteractions("/mnt/raid/ctcf_prediction_anal/trios_new_ctcf/ctcf_named/output/NA19239.bedpe") # 2
-interactions_all["NA19240"] = loadInteractions("/mnt/raid/ctcf_prediction_anal/trios_new_ctcf/ctcf_named/output/NA19240.bedpe") # 3
-interactions_all["GM12878"] = loadInteractions("/mnt/raid/ctcf_prediction_anal/trios_new_ctcf/ctcf_named/output/GM12878.bedpe") # 4
+interactions_all[0] = loadInteractions("/mnt/raid/ctcf_prediction_anal/trios_new_ctcf/ctcf_named/output/NA19238.bedpe") # 1
+interactions_all[1] = loadInteractions("/mnt/raid/ctcf_prediction_anal/trios_new_ctcf/ctcf_named/output/NA19239.bedpe") # 2
+interactions_all[2] = loadInteractions("/mnt/raid/ctcf_prediction_anal/trios_new_ctcf/ctcf_named/output/NA19240.bedpe") # 3
+interactions_all[3] = loadInteractions("/mnt/raid/ctcf_prediction_anal/trios_new_ctcf/ctcf_named/output/GM12878.bedpe") # 4
 print("--- Loaded in %s seconds ---" % (time.time() - start_time))
 start_time = time.time()
 
 all_combinations = list(itertools.product([0, 1], repeat=4))
 all_combinations = sorted(all_combinations, key=lambda x: sum(list(x)))
-my_dict = dict()
 
+threads = 16
+task_list = list()
 for combination in all_combinations:
-    interactions_list = list()
-    if(combination[0] == 1):
-        interactions_list.append(interactions_all["NA19238"])
-    if(combination[1] == 1):
-        interactions_list.append(interactions_all["NA19239"])
-    if(combination[2] == 1):
-        interactions_list.append(interactions_all["NA19240"])
-    if(combination[3] == 1):
-        interactions_list.append(interactions_all["GM12878"])
-    my_dict[combination] = len(getOverlappingMultiple(interactions_list))
+    task_list.append((interactions_all, combination))
+pool = mp.Pool(threads)
+results = pool.map(getSet, task_list)
+pool.close()
+pool.join() 
+
+labels = dict()
+for result in results:
+    labels[result[0]] = str(len(result[1]))
+
 print("--- Combinations calculated in %s seconds ---" % (time.time() - start_time))
 start_time = time.time()
-
-dictlist = list()
-for key, value in my_dict.items():
-    temp = (key,value)
-    dictlist.append(temp)
-dictlist = sorted(dictlist, key=lambda x: sum(list(x[0])))
-labels = dict()
-for item in dictlist:
-    to_remove = sum(x[1] for x in dictlist if sum_tuple(item[0]) == compare_touples4(item[0], x[0]) and item[0] != x[0])
-    item = (item[0], item[1]-to_remove)
-    labels["".join(map(str, item[0]))] = item[1]
 
 fig, ax = venn.venn4(labels, names=['NA19238', 'NA19239', 'NA19240', 'GM12878'])
 fig.savefig('venn4.png', bbox_inches='tight')
