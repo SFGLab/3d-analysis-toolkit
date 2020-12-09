bf7c2853aa4e690af2da0cf4c616a8714bc24cfb
Mateusz Chilinski <mateuszchilinski@k165.mini.pw.edu.pl>
2020-12-09 19:31:47 +0100

    snps - to test

--- find_motifs.py
+++ find_motifs.py
@@ -3,10 +3,77 @@ from Bio import SeqIO, motifs
 import numpy as np
 from sortedcontainers import SortedList
 import time 
+import vcf
+
+class SNP():
+    def __init__(self, record=None, chrm=None, pos=None):
+        if(record):
+            self.chr = record.CHROM.split("chr")[1]
+            try:
+                self.chr = int(self.chr)
+            except:
+                self.chr = ord(self.chr)
+            if(len(record.ALT) > 1):
+                self.alleles = [record.ALT[0].sequence, record.ALT[1].sequence]
+            elif(record.nucl_diversity > 0.99):
+                self.alleles = [record.REF, record.ALT[0].sequence]
+            elif(record.nucl_diversity < 0.01):
+                self.alleles = [record.ALT[0].sequence, record.ALT[0].sequence]
+
+            self.pos = record.POS
+        else:
+            self.chr = chrm.split("chr")[1]
+            self.pos = pos
+        
+    def __lt__(self, other):
+        return self.chr < other.chr or (self.chr == other.chr and self.pos < other.pos)
+
+def updateSequence(seq, snps, toRemove, id):
+    for snp in snps:
+        relativePos = snp.pos-toRemove
+        seq[relativePos] = snps[id]
+    return seq
+
+def decideOne(pssm, seq1, seq2):
+    with np.errstate(invalid='ignore'):
+            search_results1 = [(x,y) for x,y in pssm.search(seq1, threshold=7.0)]
+            search_results2 = [(x,y) for x,y in pssm.search(seq2, threshold=7.0)]
+    for r1 in search_results1:
+        for r2 in search_results2:
+            if(r1[0] >= 0 and r2[0] < 0): # >........<
+                return True
+            if(r1[1] >= 9.0 and r2[1] >= 9.0):
+                if(r1[0]*r2[0] >= 0): # >.........> or <.........<
+                    return True
+            if(r1[1] >= 12.0 and r2[1] >= 12.0):
+                if(r1[0] < 0 and r2[0] >= 0): # <.........>
+                    return True
+    return False
+def decideInteraction(pssm, seq1, seq2, snps1, snps2, toRemove1, toRemove2):
+    # maternal / first -> 1|1
+    seq1_left = updateSequence(seq1, snps1, toRemove1, 0)
+    seq2_left = updateSequence(seq1, snps1, toRemove2, 0)
+    if(decideOne(pssm, seq1_left, seq2_left)):
+        return True
+    # paternal / second 1|1 <-
+    seq1_right = updateSequence(seq1, snps1, toRemove1, 1)
+    seq2_right = updateSequence(seq1, snps1, toRemove2, 1)    
+    if(decideOne(pssm, seq1_right, seq2_right)):
+        return True
+    return False
+
 def filterInteractionsByMotifs(interactions):
     genome = dict()
 
     pfmMatrixFile = "/mnt/raid/ctcf_prediction_anal/3d_analysis_toolkit/MA0139.1.pfm"
+    vcfFile = "/mnt/raid/trios_data/sv-callings/sv_callings_data/snp_trios/SNP_HG00512.vcf"
+    start_time = time.time()
+    snps = SortedList()
+    vcf_reader = vcf.Reader(filename=vcfFile)
+    for record in vcf_reader:
+        if "chr" in record.CHROM and not("_" in record.CHROM) and not("*" in record.CHROM) and not("EBV" in record.CHROM) and not(record.is_indel):
+            snps.add(SNP(record))
+    print("--- Loading SNPs executed in %s seconds ---" % (time.time() - start_time))
 
     for record in SeqIO.parse("/mnt/raid/trios_data/GRCh38_full_analysis_set_plus_decoy_hla.fa", "fasta"):
         if("_" in record.id or "*" in record.id or "EBV" in record.id):
@@ -21,37 +88,23 @@ def filterInteractionsByMotifs(interactions):
 
     interactions_with_motif = SortedList([], key=orderInt)
     for interaction in interactions:
-        sequence1 = genome[interaction.chr1][interaction.pos1-extension:interaction.end1+extension]
-        sequence2 = genome[interaction.chr1][interaction.pos1-extension:interaction.end1+extension]
-        with np.errstate(invalid='ignore'):
-            search_results1 = [(x,y) for x,y in pssm.search(sequence1, threshold=7.0)]
-            search_results2 = [(x,y) for x,y in pssm.search(sequence2, threshold=7.0)]
 
-        added = False
-        for r1 in search_results1:
-            for r2 in search_results2:
-                if(r1[0] >= 0 and r2[0] < 0): # >........<
-                    interactions_with_motif.add(interaction)
-                    added = True
-                    break
-                if(r1[1] >= 9.0 and r2[1] >= 9.0):
-                    if(r1[0]*r2[0] >= 0): # >.........> or <.........<
-                        interactions_with_motif.add(interaction)
-                        added = True
-                        break
-                if(r1[1] >= 12.0 and r2[1] >= 12.0):
-                    if(r1[0] < 0 and r2[0] >= 0): # <.........>
-                        interactions_with_motif.add(interaction)
-                        added = True
-                        break
-            if(added):
-                break
+        all_snps_seq1 = snps[snps.bisect_left(SNP(None, interaction.chr1, interaction.pos1)):snps.bisect_right(SNP(None, interaction.chr1, interaction.end1))]
+        all_snps_seq2 = snps[snps.bisect_left(SNP(None, interaction.chr2, interaction.pos2)):snps.bisect_right(SNP(None, interaction.chr2, interaction.end2))]
+
+        sequence1 = genome[interaction.chr1][interaction.pos1-extension:interaction.end1+extension]
+        sequence2 = genome[interaction.chr2][interaction.pos2-extension:interaction.end2+extension]
+        
+        if(decideInteraction(pssm, sequence1, sequence2, all_snps_seq1, all_snps_seq2, interaction.pos1+1, interaction.pos2+1)):
+            interactions_with_motif.add(interaction)
+
     return interactions_with_motif
 
 def main():
     maxLength = 500000
 
     start_time = time.time()
+
     interactionsFile = "/mnt/raid/ctcf_prediction_anal/GM_comparisons_tries/GM12878_R2.bedpe"
     interactions = loadInteractions(interactionsFile, maxLength=0)
     print("All interactions:" + str(len(interactions)))
