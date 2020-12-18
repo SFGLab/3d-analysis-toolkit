95670b549a3796ec95d570076fc6f17c529fe244
Mateusz Chilinski <mateuszchilinski@k165.mini.pw.edu.pl>
2020-12-18 14:55:37 +0100

    update, minor improvements

--- call_ccds.py
+++ call_ccds.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
 import itertools
-from operator import add
+from operator import add, itemgetter
 import time
+import multiprocessing as mp
 
 def get_ccds(filename, output_filename, min_loops, min_diff, min_length):
     loops = pd.read_csv(filename, sep="\t", names=["chr1", "pos1", "end1", "chr2", "pos2", "end2", "pet"])
@@ -12,34 +13,55 @@ def get_ccds(filename, output_filename, min_loops, min_diff, min_length):
     loops = loops.sort_values(["chr", "start", "end"])
     ccds = list()
 
+    loops_parsed = dict()
     for chromosome in loops["chr"].unique():
-        temp = [0]*(loops.loc[loops["chr"] == chromosome]["end"].max()+1)
         loops_current_chr = list()
         for key, loop in loops.loc[loops["chr"] == chromosome].iterrows():
             loops_current_chr.append((loop["start"], loop["end"]))
-        for loop in loops_current_chr:
-            temp[loop[0]:loop[1]+1] = map(add, temp[loop[0]:loop[1]+1], ([1]*(loop[1]-loop[0]+1)))
-        current_ccd_left = 0
-        for i in range(0, len(temp)):
-            diff = abs(temp[i]-temp[i-1])
-            if(current_ccd_left == 0):
-                if(temp[i] >= min_loops or diff >= min_diff):
-                    current_ccd_left = i
-            else:
-                if(temp[i] < min_diff or diff > temp[i]):
-                    ccds.append((chromosome, current_ccd_left, i-1))
-                    current_ccd_left = 0
-        break
+        loops_parsed[chromosome] = loops_current_chr
+
+    task_list = [(loops, chromosome, min_loops, min_diff) for chromosome, loops in loops_parsed.items()]
+
+    threads = min(len(task_list), 24)
+    pool = mp.Pool(threads)
+    results = pool.map(parse_chrom, task_list)
+    pool.close()
+    pool.join() 
+
+    
     with open(output_filename, 'w') as file:
-        for ccd in ccds:
-            if(ccd[2]-ccd[1] >= min_length):
-                file.write(ccd[0]+"\t"+str(ccd[1])+"\t"+str(ccd[2])+"\n")
-            
+        for chrom, result in results:
+            for ccd in result:
+                if(ccd[1]-ccd[0] >= min_length):
+                    file.write(chrom+"\t"+str(ccd[0])+"\t"+str(ccd[1])+"\n")
 
+def parse_chrom(args):
+    loops_current_chr, chromosome, min_loops, min_diff = args
 
+    ccds_current_chr = list()
 
-filename = "hg19/GSM1872886_GM12878_CTCF_PET_clusters_fltered_cut.bedpe"
+    temp = [0]*(max(loops_current_chr, key=itemgetter(1))[0]+1)
 
-start_time_total = time.time()
-get_ccds(filename, "hg19/ccds.bed", 3, 2, 10000)
-print("--- Executed in %s seconds ---" % (time.time() - start_time_total))
+    for loop in loops_current_chr:
+        temp[loop[0]:loop[1]+1] = map(add, temp[loop[0]:loop[1]+1], ([1]*(loop[1]-loop[0]+1)))
+    current_ccd_left = 0
+    for i in range(0, len(temp)):
+        diff = abs(temp[i]-temp[i-1])
+        if(current_ccd_left == 0):
+            if(temp[i] >= min_loops or diff >= min_diff):
+                current_ccd_left = i
+        else:
+            if(temp[i] < min_diff or diff > temp[i]):
+                ccds_current_chr.append((current_ccd_left, i-1))
+                current_ccd_left = 0
+    return (chromosome, ccds_current_chr)
+
+def main():
+    filename = "hg19/GSM1872886_GM12878_CTCF_PET_clusters_fltered_cut.bedpe"
+
+    start_time_total = time.time()
+    get_ccds(filename, "hg19/ccds.bed", 3, 2, 10000)
+    print("--- Executed in %s seconds ---" % (time.time() - start_time_total))
+
+if __name__ == "__main__":
+    main()
--- find_motifs.py
+++ find_motifs.py
@@ -48,31 +48,36 @@ def decideOne(pssm, seq1, seq2):
                 if(r1[0] < 0 and r2[0] >= 0): # <.........>
                     return True
     return False
-def decideInteraction(pssm, seq1, seq2, snps1, snps2, toRemove1, toRemove2):
-    # maternal / first -> 1|1
-    seq1_left = updateSequence(seq1._data, snps1, toRemove1, 0)
-    seq2_left = updateSequence(seq2._data, snps2, toRemove2, 0)
-    if(decideOne(pssm, seq1_left, seq2_left)):
-        return True
-    # paternal / second 1|1 <-
-    seq1_right = updateSequence(seq1._data, snps1, toRemove1, 1)
-    seq2_right = updateSequence(seq2._data, snps2, toRemove2, 1)    
-    if(decideOne(pssm, seq1_right, seq2_right)):
-        return True
-    return False
+def decideInteraction(pssm, seq1, seq2, snps1=None, snps2=None, toRemove1=None, toRemove2=None):
+    if not(snps1):
+        if(decideOne(pssm, seq1, seq2)):
+            return True
+        return False
+    else:
+        # maternal / first -> 1|1
+        seq1_left = updateSequence(seq1._data, snps1, toRemove1, 0)
+        seq2_left = updateSequence(seq2._data, snps2, toRemove2, 0)
+        if(decideOne(pssm, seq1_left, seq2_left)):
+            return True
+        # paternal / second 1|1 <-
+        seq1_right = updateSequence(seq1._data, snps1, toRemove1, 1)
+        seq2_right = updateSequence(seq2._data, snps2, toRemove2, 1)    
+        if(decideOne(pssm, seq1_right, seq2_right)):
+            return True
+        return False
 
-def filterInteractionsByMotifs(interactions):
+def filterInteractionsByMotifs(interactions, vcfFile=None):
     genome = dict()
 
     pfmMatrixFile = "/mnt/raid/ctcf_prediction_anal/3d_analysis_toolkit/MA0139.1.pfm"
-    vcfFile = "/mnt/raid/trios_data/sv-callings/sv_callings_data/snp_trios/SNP_HG00512.vcf"
     start_time = time.time()
-    snps = SortedList()
-    vcf_reader = vcf.Reader(filename=vcfFile)
-    for record in vcf_reader:
-        if "chr" in record.CHROM and not("_" in record.CHROM) and not("*" in record.CHROM) and not("EBV" in record.CHROM) and not(record.is_indel):
-            snps.add(SNP(record))
-    print("--- Loading SNPs executed in %s seconds ---" % (time.time() - start_time))
+    if(vcfFile):
+        snps = SortedList()
+        vcf_reader = vcf.Reader(filename=vcfFile)
+        for record in vcf_reader:
+            if "chr" in record.CHROM and not("_" in record.CHROM) and not("*" in record.CHROM) and not("EBV" in record.CHROM) and not(record.is_indel):
+                snps.add(SNP(record))
+        print("--- Loading SNPs executed in %s seconds ---" % (time.time() - start_time))
 
     for record in SeqIO.parse("/mnt/raid/trios_data/GRCh38_full_analysis_set_plus_decoy_hla.fa", "fasta"):
         if("_" in record.id or "*" in record.id or "EBV" in record.id):
@@ -87,15 +92,16 @@ def filterInteractionsByMotifs(interactions):
 
     interactions_with_motif = SortedList([], key=orderInt)
     for interaction in interactions:
-
-        all_snps_seq1 = snps[snps.bisect_left(SNP(None, interaction.chr1, interaction.pos1)):snps.bisect_right(SNP(None, interaction.chr1, interaction.end1))]
-        all_snps_seq2 = snps[snps.bisect_left(SNP(None, interaction.chr2, interaction.pos2)):snps.bisect_right(SNP(None, interaction.chr2, interaction.end2))]
-
         sequence1 = genome[interaction.chr1][interaction.pos1-extension:interaction.end1+extension]
         sequence2 = genome[interaction.chr2][interaction.pos2-extension:interaction.end2+extension]
-        
-        if(decideInteraction(pssm, sequence1, sequence2, all_snps_seq1, all_snps_seq2, interaction.pos1+1, interaction.pos2+1)):
-            interactions_with_motif.add(interaction)
+        if(vcfFile):
+            all_snps_seq1 = snps[snps.bisect_left(SNP(None, interaction.chr1, interaction.pos1)):snps.bisect_right(SNP(None, interaction.chr1, interaction.end1))]
+            all_snps_seq2 = snps[snps.bisect_left(SNP(None, interaction.chr2, interaction.pos2)):snps.bisect_right(SNP(None, interaction.chr2, interaction.end2))]
+            if(decideInteraction(pssm, sequence1, sequence2, all_snps_seq1, all_snps_seq2, interaction.pos1+1, interaction.pos2+1)):
+                interactions_with_motif.add(interaction)
+        else:
+            if(decideInteraction(pssm, sequence1, sequence2)):
+                interactions_with_motif.add(interaction)
 
     return interactions_with_motif
 
@@ -105,9 +111,11 @@ def main():
     start_time = time.time()
 
     interactionsFile = "/mnt/raid/ctcf_prediction_anal/trios_new_ctcf/ctcf_named/output/HG00512.bedpe"
+    vcfFile = None # "/mnt/raid/trios_data/sv-callings/sv_callings_data/snp_trios/SNP_HG00512.vcf"
+
     interactions = loadInteractions(interactionsFile, maxLength=0)
     print("All interactions:" + str(len(interactions)))
-    interactions_with_motif = filterInteractionsByMotifs(interactions)
+    interactions_with_motif = filterInteractionsByMotifs(interactions, vcfFile)
 
     print("Interactions with motif: " + str(len(interactions_with_motif)))
     print("--- Executed in %s seconds ---" % (time.time() - start_time))
